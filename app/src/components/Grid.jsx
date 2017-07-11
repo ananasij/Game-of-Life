c6605570d31b81ef7a66416861a55e1ef1a5cd20
Hanna Senkevich <hanna.senkevich@gmail.com>
2017-07-11 16:00:34 +0200

    Calculating and rendering next generation on grid click

--- app/src/components/Grid.jsx
+++ app/src/components/Grid.jsx
@@ -16,41 +16,109 @@ class Grid extends React.Component {
 
     initGrid() {
         const { width, height } = this.state;
-        const initialPopulation = (width * height) / 5;
+        const initialPopulation = (width * height) / 4;
         let x;
         let y;
         let count = 0;
         const grid = [];
 
-        for (let i = 0; i < height; i += 1) {
+        for (let i = 0; i < width; i += 1) {
             grid[i] = [];
-            for (let j = 0; j < width; j += 1) {
+            for (let j = 0; j < height; j += 1) {
                 grid[i][j] = 0;
             }
         }
+
         while (count < initialPopulation) {
             x = Math.floor(Math.random() * width);
             y = Math.floor(Math.random() * height);
-            if (!grid[y][x]) {
-                grid[y][x] = 1;
+            if (!grid[x][y]) {
+                grid[x][y] = 1;
                 count += 1;
             }
         }
-
         this.setState({ currentGrid: grid });
+    }
+
+    calculateNextGenerationGrid() {
+        const newGenerationGrid = [];
+        for (let i = 0; i < this.state.width; i += 1) {
+            newGenerationGrid[i] = [];
+            for (let j = 0; j < this.state.height; j += 1) {
+                newGenerationGrid[i][j] = this.calculateNextGenerationCell(i, j);
+            }
+        }
+        this.setState({ currentGrid: newGenerationGrid });
+    }
+
+    calculateNextGenerationCell(x, y) {
+        const neighboursNumber = this.countCellNeighbours(x, y);
+
+        if ((this.getCell(x, y) && (neighboursNumber >= 2 && neighboursNumber <= 3)) ||
+            (!this.getCell(x, y) && neighboursNumber === 3)) {
+            return 1;
+        }
+        return 0;
+    }
+
+    countCellNeighbours(x, y) {
+        let count = 0;
+        let correctedX;
+        let correctedY;
+        for (let i = -1; i <= 1; i += 1) {
+            for (let j = -1; j <= 1; j += 1) {
+                correctedX = this.correctCoordinate(x, i, this.state.width);
+                correctedY = this.correctCoordinate(y, j, this.state.height);
+
+                if (this.getCell(correctedX, correctedY)) {
+                    count += 1;
+                }
+            }
+        }
+
+        if (this.getCell(x, y)) {
+            count -= 1;
+        }
+
+        return count;
+    }
+
+    correctCoordinate(coordinate, offset, length) {
+        let corrected = coordinate + offset;
+
+        if (corrected === -1) {
+            corrected = length - 1;
+        } else if (corrected === length) {
+            corrected = 0;
+        }
+
+        return corrected;
+    }
+
+    getCell(x, y) {
+        return this.state.currentGrid[x][y];
     }
 
     render() {
         const renderedGrid = [];
-        for (let i = 0; i < this.state.height; i += 1) {
+
+        for (let y = 0; y < this.state.height; y += 1) {
             const row = [];
-            for (let j = 0; j < this.state.width; j += 1) {
-                row.push(<div className={'grid-cell'.concat((this.state.currentGrid[i][j] ? ' grid-cell-alive' : ' grid-cell-dead'))} />);
+            for (let x = 0; x < this.state.width; x += 1) {
+                row.push(
+                    <div
+                        key={''.concat(x, y)}
+                        className={'grid-cell'.concat((this.getCell(x, y) ? ' grid-cell-alive' : ' grid-cell-dead'))}
+                    />
+                );
             }
-            renderedGrid.push(<div className="grid-row">{row}</div>);
+            renderedGrid.push(<div className="grid-row" key={y}>{row}</div>);
         }
+
         return (
-            <div>
+            <div
+                onClick={() => this.calculateNextGenerationGrid()}
+            >
                 {renderedGrid}
             </div>
         );
